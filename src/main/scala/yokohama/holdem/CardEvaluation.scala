8cfdc71f38d16d448d125d0b291875239bb051e1
Jeff Hemminger <jeff@atware.co.jp>
2015-07-22 16:59:18 +0900

    I added a simple logback configuration. Also implemented a rather rough evaluation strategy

--- src/main/scala/yokohama/holdem/CardEvaluation.scala
+++ src/main/scala/yokohama/holdem/CardEvaluation.scala
@@ -2,8 +2,11 @@ package yokohama.holdem
 
 import yokohama.holdem.Cards._
 import scala.util.Sorting.quickSort
+import org.slf4j.LoggerFactory
 
-object CardEvaluation {
+case class CardEvaluation() {
+
+  val logger = LoggerFactory.getLogger("yokohama.holdem.CardEvaluator");
 
   def determineHighCard(hand: Set[Card]): Value = {
     val h = hand.toArray
@@ -12,7 +15,30 @@ object CardEvaluation {
   }
 
   def assessFullHand(hand: FullHand): Value = {
-    HighCard(1)
+
+
+
+    def checkTheRest(hand: FullHand): Value = {
+      HighCard(1)
+    }
+
+    val h = hand.toArray
+    quickSort(h)
+    // map to histogram
+    val m = h.toList.groupBy { a => valueOf(a.rank) }.mapValues { b => b.size }
+    val g = m.values.toArray
+    quickSort(g)
+    val x = g.toList.reverse
+
+    x match {
+      case x :: xs if x == 4 && xs == List(1) => FourOfKind
+      case x :: xs if x == 3 && xs == List(2)  => FullHouse
+      case x :: xs if x == 3 && xs == List(1, 1) => ThreeOfKind
+      case x :: xs if x == 2 && xs == List(2, 1) => TwoPair
+      case x :: xs if x == 2 && xs == List(1, 1, 1) => Pair
+      case _ => checkTheRest(h.toSet)
+    }
+
   }
 
   def evaluate(hand: FullHand): Value = hand.toList.size match {
