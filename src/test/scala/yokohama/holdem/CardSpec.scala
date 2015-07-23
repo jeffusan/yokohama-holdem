16627a39bca80cd61f48310900763fa5b3cd665f
Yosuke Mizutani <mogproj@gmail.com>
2015-07-24 03:21:27 +0900

    added more tests

--- src/test/scala/yokohama/holdem/CardSpec.scala
+++ src/test/scala/yokohama/holdem/CardSpec.scala
@@ -23,6 +23,18 @@ class CardSpec extends FlatSpec with MustMatchers {
     an[IllegalArgumentException] must be thrownBy CardEvaluation().evaluate(hand)
   }
 
+  "Straight flush cards of ace to ten matching values" should "evaluate RoyalStraightFlush" in pendingUntilFixed {
+    val hand = Set(SA, SK, SQ, SJ, S10)
+    val response = CardEvaluation().evaluate(hand)
+    response mustBe RoyalStraightFlush
+  }
+
+  "Cards of straight and same suit matching values" should "evaluate StraightFlush" in pendingUntilFixed {
+    val hand = Set(S6, S5, S4, S3, S2)
+    val response = CardEvaluation().evaluate(hand)
+    response mustBe StraightFlush
+  }
+
   "Four matching values" should "evaluate to FourOfKind" in {
     val hand = Set(D1, H1, S1, C1, CK)
     val response = CardEvaluation().evaluate(hand)
@@ -33,6 +45,18 @@ class CardSpec extends FlatSpec with MustMatchers {
     val hand = Set(D1, H1, S1, CK, DK)
     val response = CardEvaluation().evaluate(hand)
     response mustBe FullHouse
+  }
+
+  "Cards of same suit matching values" should "evaluate Flush" in pendingUntilFixed {
+    val hand = Set(S7, S5, S4, S3, S2)
+    val response = CardEvaluation().evaluate(hand)
+    response mustBe Flush
+  }
+
+  "Cards of straight matching values" should "evaluate Straight" in pendingUntilFixed {
+    val hand = Set(S6, D5, H4, C3, S2)
+    val response = CardEvaluation().evaluate(hand)
+    response mustBe Straight
   }
 
   "Three of a kind" should "evaluate to ThreeOfKind" in {
@@ -51,6 +75,12 @@ class CardSpec extends FlatSpec with MustMatchers {
     val hand = Set(D1, H1, S10, CK, DQ)
     val response = CardEvaluation().evaluate(hand)
     response mustBe Pair
+  }
+
+  "No pair" should "evaluate to HighCard" in pendingUntilFixed {
+    val hand = Set(D9, H3, S10, CK, DQ)
+    val response = CardEvaluation().evaluate(hand)
+    response mustBe HighCard(13)
   }
 
 }
